4797a0d4e6e5f52598331e0eb4774df1fb1c1cce
RedCrafter07 <gh@r07.dev>
2024-12-01 16:59:49 +0100

    Validate login

--- packages/interface/src/routes/login/+page.server.ts
+++ packages/interface/src/routes/login/+page.server.ts
@@ -1,10 +1,88 @@
+import { error, fail, redirect } from '@sveltejs/kit';
+import axios, { AxiosError } from 'axios';
+import * as jwt from '$lib/jwt';
+import type { User } from '$lib/schemas/user.js';
+
+export const load = ({ cookies }) => {
+    if (cookies.get('token')) {
+        throw redirect(307, '/');
+    }
+};
+
 export const actions = {
-	default: async ({ request, cookies }) => {
-		const data = await request.formData();
+    default: async ({ request, cookies }) => {
+        const data = await request.formData();
 
-		const mail = data.get('email');
-		const password = data.get('password');
+        const mail = data.get('email')?.toString();
+        const password = data.get('password')?.toString();
 
-		console.log(mail, password);
-	},
+        if (!mail || !password) {
+            return fail(403, {
+                message: 'Email or password missing',
+                mail,
+                success: false,
+            });
+        }
+
+        const userRequest = await axios
+            .get('http://localhost:2000/user', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'GET',
+                params: {
+                    email: mail,
+                },
+            })
+            .catch((e: AxiosError) => {
+                if (e.status !== 404) {
+                    throw error(500);
+                }
+            });
+
+        if (!userRequest) {
+            return fail(401, {
+                success: false,
+                message: 'Invalid login credentials',
+                mail,
+            });
+        }
+
+        const user: User = userRequest.data;
+
+        const validationRequest = await axios
+            .post(
+                'http://localhost:2000/users/validate',
+                {
+                    id: user.id,
+                    password,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .catch(() => {
+                error(500);
+            });
+
+        const { valid } = validationRequest.data;
+
+        if (!valid) {
+            return fail(401, {
+                success: false,
+                message: 'Invalid login credentials.',
+                mail,
+            });
+        }
+
+        const token = await jwt.create(user.id);
+
+        cookies.set('token', token, {
+            path: '/',
+        });
+
+        throw redirect(307, '/');
+    },
 };
--- packages/interface/src/routes/login/+page.svelte
+++ packages/interface/src/routes/login/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import IconCat from '~icons/tabler/cat';
 
-	import IconKey from '~icons/tabler/key';
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	let { form }: { form: ActionData } = $props();
 </script>
 
 <div class="w-full h-screen flex items-center justify-center bg-slate-950">
@@ -13,9 +16,12 @@
 		<div class="h-full bg-slate-50 w-px opacity-10"></div>
 		<div class="flex-1 flex">
 			<form class="flex flex-col gap-4 w-full my-auto" method="post" use:enhance>
-				<IconKey class="text-[6rem] text-slate-400 mx-auto pb-4" />
-				<input tabindex="0" type="text" name="email" placeholder="Email">
-				<input type="password" name="password" placeholder="Password">
+				<IconCat class="text-[6rem] text-slate-400 mx-auto pb-4" />
+				{#if form && !form.success && form.message}
+					<p class="text-center bg-red-500 rounded-lg p-2">{form.message}</p>
+				{/if}
+				<input tabindex="0" type="text" name="email" placeholder="Email" value={form?.mail ?? ''} required>
+				<input type="password" name="password" placeholder="Password" required>
 				<button class="w-full bg-violet-500 text-slate-800 rounded-xl p-4 focus:scale-105 focus:outline-none transition-all duration-100">Log in!</button>
 			</form>
 		</div>
