0f329f63c39434fcd8464b3929e4c53a24938626
RedCrafter07 <gh@r07.dev>
2024-12-05 21:19:02 +0100

    Redirect users to password/mail change pages if such change is required

---
+++ packages/interface/src/routes/(authed)/+layout.server.ts
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ parent }) => {
+    const data = await parent();
+
+    const user = data.user!;
+
+    if (user.requiresNewPassword) {
+        throw redirect(307, '/change-password');
+    } else if (user.requiresNewEmail) {
+        throw redirect(307, '/change-mail');
+    }
+
+    return { ...data };
+};
