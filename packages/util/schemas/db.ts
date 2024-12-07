fd90049cd0926ccd18a6c8c8c01b68b1baa73600
RedCrafter07 <gh@r07.dev>
2024-12-07 12:45:04 +0100

    Update schema

--- packages/util/schemas/db.ts
+++ packages/util/schemas/db.ts
@@ -7,7 +7,7 @@ const dbSchema = z.object({
                 .string()
                 .array()
                 .min(1)
-                .describe('The hosts that are proxied to the "To" entry'),
+                .describe('The hosts proxied to the "To" entry'),
             to: z.string().describe('The location the hosts are proxied to'),
             enforceHttps: z
                 .boolean()
@@ -24,10 +24,33 @@ const dbSchema = z.object({
                 .string()
                 .default(() => crypto.randomUUID().split('-').join('')),
         })
-        .array()
-        .describe(
-            'This will automatically override the current hosts. Hosts edited in the web dashboard will be disabled and may be selected to be deleted.'
-        ),
+        .array(),
+
+    redirectEntries: z
+        .object({
+            id: z
+                .string()
+                .default(() => crypto.randomUUID().split('-').join('')),
+            name: z.string().optional(),
+            cloudflare: z
+                .boolean()
+                .default(false)
+                .describe(
+                    'Integrate this endpoint with your CloudFlare Credentials'
+                ),
+            enforceHttps: z
+                .boolean()
+                .default(false)
+                .describe('Enforce HTTPS to this endpoint'),
+            hosts: z
+                .string()
+                .min(1)
+                .array()
+                .describe('The hosts redirected to the "To" entry'),
+            to: z.string(),
+            preservePath: z.boolean().default(false),
+        })
+        .array(),
 
     users: z
         .object({
