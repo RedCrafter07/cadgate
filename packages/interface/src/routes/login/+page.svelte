d3d2c640b8e28727f094d573290cc8b9bb744cd5
RedCrafter07 <gh@r07.dev>
2024-12-08 01:05:07 +0100

    Make login page responsive

--- packages/interface/src/routes/login/+page.svelte
+++ packages/interface/src/routes/login/+page.svelte
@@ -12,21 +12,28 @@
 </svelte:head>
 
 <div class="w-full h-screen flex items-center justify-center bg-slate-950">
-    <div class="h-3/4 w-3/4 bg-slate-900 rounded-xl flex flex-row gap-12 p-8">
+    <div
+        class="w-full h-full md:h-3/4 md:w-3/4 bg-slate-900 md:rounded-xl flex flex-col lg:flex-row gap-4 justify-center lg:justify-start lg:gap-12 p-8"
+    >
         <div
-            class="flex-1 flex flex-col text-center items-center justify-center"
+            class="lg:flex-1 flex flex-col text-center items-center justify-center"
         >
-            <h1 class="text-5xl">Welcome!</h1>
-            <h3 class="text-2xl">Please log in to use Cadgate.</h3>
+            <IconCat
+                class="text-[4rem] lg:hidden text-slate-400 mx-auto pb-4"
+            />
+            <h1 class="text-3xl xl:text-5xl">Welcome!</h1>
+            <h3 class="text-xl xl:text-2xl">Please log in to use Cadgate.</h3>
         </div>
-        <div class="h-full bg-slate-50 w-px opacity-10"></div>
-        <div class="flex-1 flex">
+        <div class="h-full bg-slate-50 w-px opacity-10 hidden lg:block"></div>
+        <div class="lg:flex-1 flex">
             <form
                 class="flex flex-col gap-4 w-full my-auto"
                 method="post"
                 use:enhance
             >
-                <IconCat class="text-[6rem] text-slate-400 mx-auto pb-4" />
+                <IconCat
+                    class="hidden lg:block lg:text-[6rem] text-slate-400 mx-auto pb-4"
+                />
                 {#if form && !form.success && form.message}
                     <p class="text-center bg-red-500 rounded-lg p-2">
                         {form.message}
