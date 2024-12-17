439d9958d13e26e60c84ac60c81027bb738497d6
RedCrafter07 <gh@r07.dev>
2024-12-17 14:59:56 +0100

    Add delete method to db

--- packages/util/db/index.ts
+++ packages/util/db/index.ts
@@ -5,64 +5,70 @@ import { Paths, PathValue } from './types/path.ts';
 
 // deno-lint-ignore no-explicit-any
 class Database<S extends ZodObject<any>> {
-	private dbPath: string;
-	private db: JsonDB;
-	private schema: S;
+    private dbPath: string;
+    private db: JsonDB;
+    private schema: S;
 
-	constructor(dbPath: string, schema: S) {
-		this.dbPath = dbPath;
-		this.schema = schema;
+    constructor(dbPath: string, schema: S) {
+        this.dbPath = dbPath;
+        this.schema = schema;
 
-		this.db = new JsonDB(
-			new Config(path.resolve(this.dbPath), true, false, '.'),
-		);
-	}
+        this.db = new JsonDB(
+            new Config(path.resolve(this.dbPath), true, false, '.')
+        );
+    }
 
-	async reload() {
-		await this.db.reload();
-	}
+    async reload() {
+        await this.db.reload();
+    }
 
-	async validate() {
-		const data = await this.db.getData('.');
-		return this.schema.safeParse(data);
-	}
+    async validate() {
+        const data = await this.db.getData('.');
+        return this.schema.safeParse(data);
+    }
 
-	async push<P extends Paths<z.infer<S>>>(
-		path: P,
-		value: PathValue<z.infer<S>, P>,
-	): Promise<void> {
-		await this.db.push(`.${path}`, value, true);
-	}
+    async push<P extends Paths<z.infer<S>>>(
+        path: P,
+        value: PathValue<z.infer<S>, P>
+    ): Promise<void> {
+        await this.db.push(`.${path}`, value, true);
+    }
 
-	// @ts-ignore: Invalid error
-	async getData<P extends Paths<z.infer<S>>>(
-		path: P,
-	): Promise<PathValue<z.infer<S>, P>> {
-		const rawData = await this.db.getData(`.${path}`);
-		const schemaAtPath = this.getSchemaAtPath(path);
-		const validatedData = schemaAtPath.parse(rawData);
-		return validatedData as PathValue<z.infer<S>, P>;
-	}
+    // @ts-ignore: Invalid error
+    async getData<P extends Paths<z.infer<S>>>(
+        path: P
+    ): Promise<PathValue<z.infer<S>, P>> {
+        const rawData = await this.db.getData(`.${path}`);
+        const schemaAtPath = this.getSchemaAtPath(path);
+        const validatedData = schemaAtPath.parse(rawData);
+        return validatedData as PathValue<z.infer<S>, P>;
+    }
 
-	private getSchemaAtPath<P extends Paths<z.infer<S>>>(path: P): z.ZodTypeAny {
-		const pathParts = path.split('.');
-		// deno-lint-ignore no-explicit-any
-		let currentSchema: any = this.schema;
+    async delete<P extends Paths<z.infer<S>>>(path: P) {
+        await this.db.delete(`.${path}`);
+    }
 
-		for (const part of pathParts) {
-			if (currentSchema instanceof z.ZodObject) {
-				currentSchema = currentSchema.shape[part];
-			} else {
-				throw new Error(`Invalid path: ${path}`);
-			}
-		}
+    private getSchemaAtPath<P extends Paths<z.infer<S>>>(
+        path: P
+    ): z.ZodTypeAny {
+        const pathParts = path.split('.');
+        // deno-lint-ignore no-explicit-any
+        let currentSchema: any = this.schema;
 
-		if (!currentSchema) {
-			throw new Error(`No schema found at path: ${path}`);
-		}
+        for (const part of pathParts) {
+            if (currentSchema instanceof z.ZodObject) {
+                currentSchema = currentSchema.shape[part];
+            } else {
+                throw new Error(`Invalid path: ${path}`);
+            }
+        }
 
-		return currentSchema;
-	}
+        if (!currentSchema) {
+            throw new Error(`No schema found at path: ${path}`);
+        }
+
+        return currentSchema;
+    }
 }
 
 export { Database };
