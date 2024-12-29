90e8a9351832b219d8c7e7c125a2bbe0170a75b2
RedCrafter07 <gh@r07.dev>
2024-12-29 15:05:30 +0100

    Add height transition component

---
+++ packages/interface/src/lib/components/HeightTransition.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { circInOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+
+	type TweenedOptions<T> = Parameters<typeof tweened<T>>[1];
+
+	const { duration, children }: { duration: number; children: Snippet } =
+		$props();
+
+	let height = $state(0);
+	let currentHeight = $state(0);
+
+	const animationConfig: TweenedOptions<number> = {
+		duration: duration,
+		easing: circInOut,
+	};
+
+	let animate = tweened(0, animationConfig);
+
+	$effect(() => {
+		if (currentHeight < height)
+			animate.set(height, { ...animationConfig, delay: duration });
+		else animate.set(height, animationConfig);
+
+		currentHeight = height;
+	});
+</script>
+
+<div style="height:{$animate}px;">
+	<div bind:offsetHeight={height}>
+		{@render children()}
+	</div>
+</div>
